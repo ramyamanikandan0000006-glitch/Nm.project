511b2464d445e6678a8724efeb156a5eb50efdf2
ramyamanikandan0000006-glitch <ramyamanikandan0000006@gmail.com>
2025-09-03 20:02:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+
+  e.preventDefault();
+
+  alert("Thank you for your message, Vinodhini will get back to you soon!");
+
+  this.reset();
+
+});
